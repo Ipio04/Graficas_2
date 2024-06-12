91bfce0dcde38df492be651bfa729a862826f30f
Ipio04 <keyelf1593@gmail.com>
2024-06-12 10:38:39 -0700

    : )

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import BarChart from './Charts/BarChart';
+import './App.css';
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+     <PieChart />
+    </div>
+  );
+}
+
+export default App;
